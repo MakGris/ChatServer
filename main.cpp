675c6bb2b0659cd8398a396a1b65beb33b7f7a83
MakGris <maxgrishenko@gmail.com>
2024-07-15 18:08:09 +0700

    add some network stuff

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstdlib>
+#include <unistd.h>
+
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int sockFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockFileDescriptor == -1) {
+        cout << "Failed to create socket" << endl;
+        exit(EXIT_FAILURE);
+    }
+    sockaddr_in sockAddr;
+    sockAddr.sin_family = AF_INET;
+    sockAddr.sin_addr.s_addr = INADDR_ANY;
+    sockAddr.sin_port = htons(9999);
+    if(bind(sockFileDescriptor, (struct sockaddr*)&sockAddr, sizeof(sockAddr))  < 0) {
+        cout << "Failed to bind port: 9999" << endl;
+    }
+    if(listen(sockFileDescriptor, 20) < 0) {
+        cout << "Failed to listen" << endl;
+    }
     return 0;
 }
